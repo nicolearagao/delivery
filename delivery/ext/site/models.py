dd9df81c0a94893cf01ab287063a43f8e931a09a
Nicole Aragao <nicole.aragao@zoop.com.br>
2021-05-24 00:08:21 -0300

    added model of table items

--- delivery/ext/site/models.py
+++ delivery/ext/site/models.py
@@ -19,11 +19,22 @@ class Category(db.Model):
 class Store(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column("name", db.Unicode, unique=True)
+    image = db.Column("image", db.Unicode)
     user_id = db.Column("user_id", db.Integer, db.ForeignKey("user.id"))
     category_id = db.Column("category_id", db.Integer, db.ForeignKey("category.id"))
     user_name = db.Column("user", db.Unicode, db.ForeignKey("user.name"))
     category_name = db.Column("category", db.Unicode, db.ForeignKey("category.name"))
+
+
+class Items(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.Unicode, unique=True)
+    image = db.Column("image", db.Unicode)
+    price = db.Column("price", db.Numeric)
+    store_id = db.Column("store_id", db.Integer, db.ForeignKey("store.id"))
+    available = db.Column("available", db.Boolean)
     
+
 
 
 
